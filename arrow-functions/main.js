c7bd11c2c8039e6f7d843503788e2e90680f2ed3
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-10-30 16:50:01 +0200

    Use short hand arrow function

--- arrow-functions/main.js
+++ arrow-functions/main.js
@@ -1,8 +1,6 @@
 var log = function (message) {
     console.log(message);
 };
-var doLog = function (message) {
-    console.log(message);
-};
+var doLog = function (message) { return console.log(message); };
 log("from log function");
 doLog("from log arrow function");
--- arrow-functions/main.ts
+++ arrow-functions/main.ts
@@ -2,9 +2,7 @@ let log = function(message) {
     console.log(message);
 };
 
-let doLog = (message) => {
-    console.log(message);
-};
+let doLog = (message) => console.log(message);
 
 log("from log function");
 doLog("from log arrow function");
