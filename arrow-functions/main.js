835697a4578d46ca9d793425293bced82723e387
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-10-30 16:51:41 +0200

    Use arrow function witout params

--- arrow-functions/main.js
+++ arrow-functions/main.js
@@ -2,5 +2,7 @@ var log = function (message) {
     console.log(message);
 };
 var doLog = function (message) { return console.log(message); };
+var doLog2 = function () { return console.log("Log2"); };
 log("from log function");
 doLog("from log arrow function");
+doLog2();
--- arrow-functions/main.ts
+++ arrow-functions/main.ts
@@ -3,6 +3,8 @@ let log = function(message) {
 };
 
 let doLog = (message) => console.log(message);
+let doLog2 = () => console.log("Log2");
 
 log("from log function");
 doLog("from log arrow function");
+doLog2();
