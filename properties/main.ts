2d23290b76d9dcd63e93ce03127e857a344644f5
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-11-02 15:49:37 +0200

    Change property casing style

--- properties/main.ts
+++ properties/main.ts
@@ -1,26 +1,26 @@
 class Point {
-    constructor(private x?: number, private y?: number) {
+    constructor(private _x?: number, private _y?: number) {
     }
 
     draw(): void {
-        console.log("X: " + this.x + ", Y: " + this.y);
+        console.log("X: " + this._x + ", Y: " + this._y);
     }
 
-    get X(): number {
-        return this.x;
+    get x(): number {
+        return this._x;
     }
 
-    set X(value: number) {
+    set x(value: number) {
         if(value < 0) {
             throw new Error("value cannot be less than 0");
         }
 
-        this.x = value;
+        this._x = value;
     }
 }
 
 let point = new Point(1, 2);
-let x: number = point.X;
-point.X = 10;
+let x: number = point.x;
+point.x = 10;
 console.log(x);
 point.draw();
