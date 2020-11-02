7c92ac5598aee4eab64ed1140e25c4b7fd333be8
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-11-02 15:59:34 +0200

    Use modules

--- modules/main.js
+++ modules/main.js
@@ -1,12 +1,5 @@
-var Point = /** @class */ (function () {
-    function Point(_x, _y) {
-        this._x = _x;
-        this._y = _y;
-    }
-    Point.prototype.draw = function () {
-        console.log("X: " + this._x + ", Y: " + this._y);
-    };
-    return Point;
-}());
-var point = new Point(1, 2);
+"use strict";
+exports.__esModule = true;
+var point_1 = require("./point");
+var point = new point_1.Point(1, 2);
 point.draw();
