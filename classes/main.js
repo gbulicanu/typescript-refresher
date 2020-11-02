3e8c6b254f1fe2386d20457bb8feb33b6031dcf6
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-11-02 10:22:37 +0200

    Add final js code for classes module

--- classes/main.js
+++ classes/main.js
@@ -4,9 +4,9 @@ var Point = /** @class */ (function () {
     Point.prototype.draw = function () {
         // ...
     };
-    ;
     Point.prototype.getDistance = function (another) {
         // ...
+        return 0;
     };
     return Point;
 }());
