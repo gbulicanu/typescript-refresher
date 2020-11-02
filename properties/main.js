7ece702aba5ef48e9971998d5f51b99d2649cb08
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-11-02 15:42:42 +0200

    Add setter method

--- properties/main.js
+++ properties/main.js
@@ -9,9 +9,16 @@ var Point = /** @class */ (function () {
     Point.prototype.getX = function () {
         return this.x;
     };
+    Point.prototype.setX = function (value) {
+        if (value < 0) {
+            throw new Error("value cannot be less than 0");
+        }
+        this.x = value;
+    };
     return Point;
 }());
 var point = new Point(1, 2);
 var x = point.getX();
+point.setX(10);
 console.log(x);
 point.draw();
--- properties/main.ts
+++ properties/main.ts
@@ -6,12 +6,21 @@ class Point {
         console.log("X: " + this.x + ", Y: " + this.y);
     }
 
-    getX(): number{
+    getX(): number {
         return this.x;
+    }
+
+    setX(value: number): void {
+        if(value < 0) {
+            throw new Error("value cannot be less than 0");
+        }
+
+        this.x = value;
     }
 }
 
 let point = new Point(1, 2);
 let x = point.getX();
+point.setX(10);
 console.log(x);
 point.draw();
